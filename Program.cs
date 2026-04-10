886b26b13e9411e1a9f0222143f7996200838d24
ms4tt4 <muhmd.s.abdullahi@gmail.com>
2026-04-10 08:54:50 +0100

    appsettings.json related changes

--- Program.cs
+++ Program.cs
@@ -74,5 +74,76 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+    if (!context.Plans.Any())
+    {
+        Console.WriteLine("Seeding plans into database..");
+        context.Plans.AddRange(new Plan
+        {
+            Id = 1,
+            Name = "Mobile",
+            Price = 2500,
+            VideoQuality = "Fair",
+            Resolution = "480p",
+            SpatialAudio = false,
+            MaxDevices = 1,
+            MaxDownloadDevices = 1,
+            AllowedDeviceTypes = "mobile,tablet",
+            Description = "Mobile devices only",
+            PaystackPlanCode = "PLN_mobile_monthly"
+        },
+        new Plan
+        {
+            Id = 2,
+            Name = "Basic",
+            Price = 4000,
+            VideoQuality = "Good",
+            Resolution = "720p",
+            SpatialAudio = false,
+            MaxDevices = 1,
+            MaxDownloadDevices = 1,
+            AllowedDeviceTypes = "tv,computer,mobile,tablet",
+            Description = "HD streaming on 1 device",
+            PaystackPlanCode = "PLN_basic_monthly"
+        },
+        new Plan
+        {
+            Id = 3,
+            Name = "Standard",
+            Price = 6500,
+            VideoQuality = "Great",
+            Resolution = "1080p",
+            SpatialAudio = false,
+            MaxDevices = 2,
+            MaxDownloadDevices = 2,
+            AllowedDeviceTypes = "tv,computer,mobile,tablet",
+            Description = "Full HD streaming, 2 devices",
+            PaystackPlanCode = "PLN_standard_monthly"
+        },
+        new Plan
+        {
+            Id = 4,
+            Name = "Premium",
+            Price = 8500,
+            VideoQuality = "Best",
+            Resolution = "4K + HDR",
+            SpatialAudio = true,
+            MaxDevices = 4,
+            MaxDownloadDevices = 6,
+            AllowedDeviceTypes = "tv,computer,mobile,tablet",
+            Description = "Ultra HD streaming with HDR and spatial audio",
+            PaystackPlanCode = "PLN_premium_monthly"
+        }
+        );
+        await context.SaveChangesAsync();
+        Console.WriteLine("Successfully seeded 4 subscriptions plans!");
+    }
+    else
+    {
+        Console.WriteLine($"Found {context.Plans.Count()} existing plans in database");
+    }
+}
+    app.Run();
