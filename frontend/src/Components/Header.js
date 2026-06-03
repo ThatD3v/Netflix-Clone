b381e1833265e86eee4d5d46f9161da6e674c5d1
Mubarak Amoo <mubaarakamoo@gmail.com>
2026-06-03 09:26:13 +0100

    Finished checkPage

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "../Components/Header.css";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <div className="landing-header">
       <div className="img">
         <img src="Netflix Logo/Netflix.png" alt="Netflix" width={"150px"} />
       </div>
       <div className="signIn">
-        <button>Sign in</button>
+        <button onClick={() => navigate("/check")}>Sign in</button>
       </div>
     </div>
   );
--- frontend/src/Pages/CheckPage.js
+++ frontend/src/Pages/CheckPage.js
@@ -1,7 +1,24 @@
 import "../Styles/CheckPage.css";
 
 function CheckPage() {
-  return <div></div>;
+  return (
+    <div className="checkPage">
+      <header>
+        <img src="Netflix Logo/Netflix.png" alt="Netflix" width={"150px"} />
+      </header>
+      <div className="check-section">
+        <div className="text">
+          <h1>Enter your info to sign in</h1>
+          <h3>Or get started with a new account</h3>
+        </div>
+        <form>
+          <input type="email" placeholder="Email address" />
+          <br />
+          <button>Continue</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default CheckPage;
